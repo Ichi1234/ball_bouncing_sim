0e301c9fd424aafa260ac427821d29753285b0fb
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2023-11-16 12:03:59 +0700

    still have many bug

--- run_ball.py
+++ run_ball.py
@@ -1,26 +1,31 @@
 import turtle
-import ball
+from ball import Balls
+
+class Simulation:
+
+    def __init__(self):
+        self.speed = turtle.speed("slowest")
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+        self.ball_radius = 0.05 * self.canvas_width
+
 
 num_balls = int(input("Number of balls to simulate: "))
-turtle.speed(0)
+speed = int(input("Enter the speed for the balls: "))
 turtle.tracer(0)
-turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-ball_radius = 0.05 * canvas_width
-turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls)
-while (True):
+# turtle.speed(0)
+# turtle.tracer(0)
+# canvas_width = turtle.screensize()[0]
+# canvas_height = turtle.screensize()[1]
+# ball_radius = 0.05 * canvas_width
+sim = Simulation()
+
+ball = Balls(sim.canvas_width, sim.canvas_height, sim.ball_radius, num_balls, speed)
+while True:
     turtle.clear()
     for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
+        ball.draw_circle(i)
+        ball.move_circle(i)
     turtle.update()
 
 # hold the window; close it by clicking the window close 'x' mark
